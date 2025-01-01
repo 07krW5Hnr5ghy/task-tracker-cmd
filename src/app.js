cf66d7aa525a6f3f78f0c02186128e3672133671
07krW5Hnr5ghy <jryn145@gmail.com>
2024-12-31 20:09:47 -0500

    feature(main): basic cli handling and read task function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+// tasks file route
+const JSON_FILE = path.resolve(process.cwd(),'tasks.json');
+
+// read tasks
+const readTasks = () => {
+    if(!fs.existsSync(JSON_FILE)){
+        return[];
+    }
+    const data = fs.readFileSync(JSON_FILE,'utf-8');
+    return JSON.parse(data||'[]');
+}
+
+// CLI handler
+const main = () => {
+    const [,,command,...args] = process.argv;
+    switch(command){
+        case'list':
+            listTasks(args[0]||'all');
+            break;
+        default:
+            console.log(`
+                Usage:
+                task-cli add "Task description"
+                task-cli list [all|todo|in-progress|done]
+                task-cli update <Task ID> "New Description"
+                task-cli delete <Task ID>
+                task-cli mark-in-progress <Task ID>
+                task-cli mark-done <Task ID>
+            `);
+    }
+};
+
+main();
