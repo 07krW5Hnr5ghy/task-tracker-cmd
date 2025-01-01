f76bb458140bcd24ad23abd187fbdd6d530c1bcc
07krW5Hnr5ghy <jryn145@gmail.com>
2024-12-31 20:33:49 -0500

    feature(main): add task logic finished

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,14 @@ const path = require('path');
 // tasks file route
 const JSON_FILE = path.resolve(process.cwd(),'tasks.json');
 
-// read tasks
+// tasks statuses
+const STATUSES = {
+    todo:"todo",
+    inProgress:"in progress",
+    done:"done"
+};
+
+// helper functions
 const readTasks = () => {
     if(!fs.existsSync(JSON_FILE)){
         return[];
@@ -14,12 +21,37 @@ const readTasks = () => {
     return JSON.parse(data||'[]');
 }
 
+const writeTasks = (tasks) => {
+    fs.writeFileSync(JSON_FILE,JSON.stringify(tasks,null,2),'utf-8');
+}
+
+// tasks operations
+const addTask = (description) => {
+    const tasks = readTasks();
+    const id = tasks.length ? tasks[tasks.lenght-1].id + 1 : 1;
+    const newTask = {
+        id,
+        description,
+        status:STATUSES.todo,
+        createdAt:new Date().toISOString(),
+        updatedAt:new Date().toISOString(),
+    };
+    tasks.push(newTask);
+    writeTasks(tasks);
+    console.log(`Task added successfully (ID:${id})`);
+}
+
+
 // CLI handler
 const main = () => {
     const [,,command,...args] = process.argv;
     switch(command){
-        case'list':
-            listTasks(args[0]||'all');
+        case'add':
+            if(args.length===1){
+                addTask(args[0]);
+            }else{
+                console.log('Usage: task-cli add "Task Description"');
+            }
             break;
         default:
             console.log(`
